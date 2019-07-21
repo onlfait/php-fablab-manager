96d865a5cab9e37a6782087cc3edc4d62dec9c21
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-21 16:11:23 +0200

    add "no members found" message

--- www/pages/members/list.php
+++ www/pages/members/list.php
@@ -3,8 +3,14 @@
 <section class="body">
   <h1>Members</h1>
   <?php $members = dbSelectArray('login', 'members'); ?>
-  <?php foreach ($members as $member) { ?>
-    <div class="membre"><?php echo($member['login']) ?></div>
+  <?php if (count($members) === 0) { ?>
+    <div class="membre">
+      <p>No members found...</p>
+    </div>
+  <?php } else { ?>
+    <?php foreach ($members as $member) { ?>
+      <div class="membre"><?php echo($member['login']) ?></div>
+    <?php } ?>
   <?php } ?>
 </section>
 <!-- end .main .body -->
