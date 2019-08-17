e75c5ffceb52850112c4573febd2b8c46591c949
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-17 15:47:10 +0200

    old base

--- www/index.php
+++ www/index.php
@@ -1,32 +1,28 @@
-<?php
-/**
- * This file is the single entry point of the application.
- *
- * This file is responsible to :
- *  - Define the main namespace.
- *  - Define some common constants.
- *  - Require and call the startup function.
- */
-namespace PFM;
+<!DOCTYPE HTML>
+<html>
+<head>
+  <?php include("FonctionsPHP/head_global.php"); ?>
+</head>
+<body>
 
-/**
- * Directory separator
- *
- * @var string Pouet tralala...
- */
-const DS = DIRECTORY_SEPARATOR;
+  <?php include("FonctionsPHP/entete.php"); ?>
+  <?php include("FonctionsPHP/menu.php"); ?>
 
-/** Absolute path to root directory */
-define('PFM\ROOT_PATH', dirname(__FILE__) . DS);
+  <div id="content" >
+    <div id="centercolumn">
+      <p class="SousTitreC">C'est quoi un Fablab</p>
+      <br>
+      <p class="TexteC"> Fablab (contraction des mots Fabrication et Laboratoire) est un lieu abordable, ouvert au public mettant à la disposition de ses utilisateurs les ressources techniques, technologiques et humaines (machines, outils, logiciels, procédés, savoir-faire, mentors) nécessaires à la conception, l’optimisation, la réparation de toute sorte d’objets.</p>
+      <br>
+      <p class="TexteC"> Un fablab c’est aussi et surtout un lieu de partage où les membres sont tour à tour bénéficiaires et contributeurs, où les expériences sont mises en communs afin d’optimiser le potentiel global d’innovation.</p>
+      <br>
+      <p class="TexteC"> Les fablabs constituent également un réseau mondial et dans cette optique de partage des connaissances (open-source/open-hardware), les fablabs de chaque pays, ville, quartier sont en contact et travaillent ensemble en partageant le fruit de leurs recherches et développements.</p>
+      <br>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/zawAJr2RZUU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div><!-- #centercolumn -->
+  </div><!-- #content -->
 
-/** Absolute path to core directory */
-define('PFM\CORE_PATH', ROOT_PATH . 'core' . DS);
+  <?php include("FonctionsPHP/pied_de_page.php"); ?>
 
-/** Absolute path to main directory */
-define('PFM\MAIN_PATH', ROOT_PATH . 'main' . DS);
-
-// Require core startup functions
-require_once(CORE_PATH . 'functions' . DS . 'startup.php');
-
-// Startup PHP Fablab Manager
-\PFM\core\functions\startup();
+</body>
+</html>
