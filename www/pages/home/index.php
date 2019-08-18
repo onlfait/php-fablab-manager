04dd54da64058b4a200cb1d9157ce8e0bb6fad8a
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 06:05:45 +0200

    missing page home

---
+++ www/pages/home/index.php
@@ -0,0 +1,13 @@
+<div id="content" >
+  <div id="centercolumn">
+    <p class="SousTitreC">C'est quoi un Fablab</p>
+    <br>
+    <p class="TexteC"> Fablab (contraction des mots Fabrication et Laboratoire) est un lieu abordable, ouvert au public mettant à la disposition de ses utilisateurs les ressources techniques, technologiques et humaines (machines, outils, logiciels, procédés, savoir-faire, mentors) nécessaires à la conception, l’optimisation, la réparation de toute sorte d’objets.</p>
+    <br>
+    <p class="TexteC"> Un fablab c’est aussi et surtout un lieu de partage où les membres sont tour à tour bénéficiaires et contributeurs, où les expériences sont mises en communs afin d’optimiser le potentiel global d’innovation.</p>
+    <br>
+    <p class="TexteC"> Les fablabs constituent également un réseau mondial et dans cette optique de partage des connaissances (open-source/open-hardware), les fablabs de chaque pays, ville, quartier sont en contact et travaillent ensemble en partageant le fruit de leurs recherches et développements.</p>
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/zawAJr2RZUU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  </div><!-- #centercolumn -->
+</div><!-- #content -->
