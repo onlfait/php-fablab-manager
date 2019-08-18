57be83d02c099841b5f0f4457dbf1e6178d8fbc0
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 06:15:51 +0200

    cleanning

--- www/pages/workshop/index.php
+++ www/pages/workshop/index.php
@@ -1,76 +1,60 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <?php include("FonctionsPHP/head_global.php"); ?>
-</head>
+<div id="content" >
+  <div id="centercolumn">
+    <p class="SousTitreC"> Espace de Travail </p>
+    <br>
 
-<body>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> Calendrier de disponibilité des machines </p>
+    <iframe src="https://dev.onlfait.ch/EventCalendar/guest.php" style="border: 0" width="100%" height="900"></iframe>
+    <p class="SousSousTitreC"><br></p>
 
-  <?php include("FonctionsPHP/entete.php"); ?>
-  <?php include("FonctionsPHP/menu.php"); ?>
+    <p class="SousSousTitreC"> Présentation des machines </p>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> Découpeuse Laser CO2 </p>
+    <img src="Image/Machine/DecoupeuseLaser.jpg" width="400">
+    <p class="TexteC"> <b>Description :</b> 1 Découpeuse Laser est disponible. Elle permet de découper/graver le bois, le plexiglass, le cuire. </p>
+    <p class="TexteC"> <b>Puissance :</b> 90W  </p>
+    <p class="TexteC"> <b>Espace de travail :</b> 1000 x 600 mm  </p>
+    <p class="TexteC"> <b>Marque / Modèle :</b> SignsTech / 1060SP  </p>
+    <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
+    <p class="TexteC"> <b>Logiciel :</b> LightBurn </p>
+    <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
 
-  <div id="content" >
-    <div id="centercolumn">
-      <p class="SousTitreC"> Espace de Travail </p>
-      <br>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> Imprimantes 3D </p>
+    <img src="Image/Machine/Imprimante3D.jpg" width="400">
+    <p class="TexteC"> <b>Description :</b> 2 imprimantes 3D est à disposition. Elles possèdent un plateau chauffant et une buse de 1.75mm </p>
+    <p class="TexteC"> <b>Espace de travail :</b> 220 x 220 x 260 mm  </p>
+    <p class="TexteC"> <b>Marque / Modèle :</b> RepRap / iTopie (Open Hardware)  </p>
+    <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
+    <p class="TexteC"> <b>Logiciel :</b> Slic3r, Pronterface, Fabrica, Cura </p>
+    <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
 
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> Calendrier de disponibilité des machines </p>
-      <iframe src="https://dev.onlfait.ch/EventCalendar/guest.php" style="border: 0" width="100%" height="900"></iframe>
-      <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> CNC (Fraiseuse numérique) </p>
+    <img src="Image/Machine/CNC.jpg" width="400">
+    <p class="TexteC"> <b>Description :</b> 1 Fraiseuse CNC est à disposition. Elle permet d'usiner du bois. </p>
+    <p class="TexteC"> <b>Broche :</b> KRESS 1500 </p>
+    <p class="TexteC"> <b>Espace de travail :</b> 800 x 700 x 150 mm  </p>
+    <p class="TexteC"> <b>Marque / Modèle :</b> OpenBuilds / OX (Open Hardware)  </p>
+    <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
+    <p class="TexteC"> <b>Logiciel :</b> LaserWeb, Fabrica (Open Source) – CamBam </p>
+    <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
 
-      <p class="SousSousTitreC"> Présentation des machines </p>
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> Découpeuse Laser CO2 </p>
-      <img src="Image/Machine/DecoupeuseLaser.jpg" width="400">
-      <p class="TexteC"> <b>Description :</b> 1 Découpeuse Laser est disponible. Elle permet de découper/graver le bois, le plexiglass, le cuire. </p>
-      <p class="TexteC"> <b>Puissance :</b> 90W  </p>
-      <p class="TexteC"> <b>Espace de travail :</b> 1000 x 600 mm  </p>
-      <p class="TexteC"> <b>Marque / Modèle :</b> SignsTech / 1060SP  </p>
-      <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
-      <p class="TexteC"> <b>Logiciel :</b> LightBurn </p>
-      <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> Découpeuse Vinyle </p>
+    <img src="Image/Machine/Decoupeuse.jpg" width="400">
+    <p class="TexteC"> <b>Description :</b> 1 Découpeuse Vinyle est à disposition. </p>
+    <p class="TexteC"> <b>Espace de travail :</b> A3 </p>
+    <p class="TexteC"> <b>Marque / Modèle :</b> Silhouette CAMEO 3 </p>
+    <p class="TexteC"> <b>Logiciel :</b> Silhouette Studio® </p>
+    <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
 
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> Imprimantes 3D </p>
-      <img src="Image/Machine/Imprimante3D.jpg" width="400">
-      <p class="TexteC"> <b>Description :</b> 2 imprimantes 3D est à disposition. Elles possèdent un plateau chauffant et une buse de 1.75mm </p>
-      <p class="TexteC"> <b>Espace de travail :</b> 220 x 220 x 260 mm  </p>
-      <p class="TexteC"> <b>Marque / Modèle :</b> RepRap / iTopie (Open Hardware)  </p>
-      <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
-      <p class="TexteC"> <b>Logiciel :</b> Slic3r, Pronterface, Fabrica, Cura </p>
-      <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
-
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> CNC (Fraiseuse numérique) </p>
-      <img src="Image/Machine/CNC.jpg" width="400">
-      <p class="TexteC"> <b>Description :</b> 1 Fraiseuse CNC est à disposition. Elle permet d'usiner du bois. </p>
-      <p class="TexteC"> <b>Broche :</b> KRESS 1500 </p>
-      <p class="TexteC"> <b>Espace de travail :</b> 800 x 700 x 150 mm  </p>
-      <p class="TexteC"> <b>Marque / Modèle :</b> OpenBuilds / OX (Open Hardware)  </p>
-      <p class="TexteC"> <b>Electronique :</b> SmoothieBoard (Open Hardware)  </p>
-      <p class="TexteC"> <b>Logiciel :</b> LaserWeb, Fabrica (Open Source) – CamBam </p>
-      <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
-
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> Découpeuse Vinyle </p>
-      <img src="Image/Machine/Decoupeuse.jpg" width="400">
-      <p class="TexteC"> <b>Description :</b> 1 Découpeuse Vinyle est à disposition. </p>
-      <p class="TexteC"> <b>Espace de travail :</b> A3 </p>
-      <p class="TexteC"> <b>Marque / Modèle :</b> Silhouette CAMEO 3 </p>
-      <p class="TexteC"> <b>Logiciel :</b> Silhouette Studio® </p>
-      <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
-
-      <p class="SousSousTitreC"><br></p>
-      <p class="SousSousTitreC"> Thermoformeuse </p>
-      <p class="TexteC"> <b>Description :</b> 1 Thermoformeuse est à disposition. </p>
-      <p class="TexteC"> <b>Espace de travail :</b> 220 x 180 mm  </p>
-      <p class="TexteC"> <b>Marque / Modèle :</b> Fait maison  </p>
-      <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
-    </div><!-- #centercolumn -->
-  </div><!-- #content -->
-
-  <?php include("FonctionsPHP/pied_de_page.php"); ?>
-
-</body>
-</html>
+    <p class="SousSousTitreC"><br></p>
+    <p class="SousSousTitreC"> Thermoformeuse </p>
+    <p class="TexteC"> <b>Description :</b> 1 Thermoformeuse est à disposition. </p>
+    <p class="TexteC"> <b>Espace de travail :</b> 220 x 180 mm  </p>
+    <p class="TexteC"> <b>Marque / Modèle :</b> Fait maison  </p>
+    <p class="TexteC"> <b>Mode d'emploi :</b> Tutorial </p>
+  </div><!-- #centercolumn -->
+</div><!-- #content -->
