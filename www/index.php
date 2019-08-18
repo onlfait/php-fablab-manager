644bb4d91354e0e3e07ea826c53af5d60827d9e3
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 06:05:56 +0200

    single entry point

--- www/index.php
+++ www/index.php
@@ -1,28 +1,12 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-  <?php include("FonctionsPHP/head_global.php"); ?>
-</head>
-<body>
+<?php
+// Absolute path to root directory (www)
+define('PFM_ROOT_PATH', dirname(__FILE__) . '/');
 
-  <?php include("FonctionsPHP/entete.php"); ?>
-  <?php include("FonctionsPHP/menu.php"); ?>
+// Require the global state
+require_once PFM_ROOT_PATH . 'state/init.php';
 
-  <div id="content" >
-    <div id="centercolumn">
-      <p class="SousTitreC">C'est quoi un Fablab</p>
-      <br>
-      <p class="TexteC"> Fablab (contraction des mots Fabrication et Laboratoire) est un lieu abordable, ouvert au public mettant à la disposition de ses utilisateurs les ressources techniques, technologiques et humaines (machines, outils, logiciels, procédés, savoir-faire, mentors) nécessaires à la conception, l’optimisation, la réparation de toute sorte d’objets.</p>
-      <br>
-      <p class="TexteC"> Un fablab c’est aussi et surtout un lieu de partage où les membres sont tour à tour bénéficiaires et contributeurs, où les expériences sont mises en communs afin d’optimiser le potentiel global d’innovation.</p>
-      <br>
-      <p class="TexteC"> Les fablabs constituent également un réseau mondial et dans cette optique de partage des connaissances (open-source/open-hardware), les fablabs de chaque pays, ville, quartier sont en contact et travaillent ensemble en partageant le fruit de leurs recherches et développements.</p>
-      <br>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/zawAJr2RZUU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    </div><!-- #centercolumn -->
-  </div><!-- #content -->
+// Require commons functions
+require_once PFM_ROOT_PATH . 'functions/commons.php';
 
-  <?php include("FonctionsPHP/pied_de_page.php"); ?>
-
-</body>
-</html>
+// Dispatch main request
+pfm_dispatch_route($_GET);
