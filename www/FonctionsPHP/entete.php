85dae4b9775fa1f3163e1581b29a432f913a77df
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 05:50:43 +0200

    entete.php is now part of layout

--- www/FonctionsPHP/entete.php
+++
@@ -1,7 +0,0 @@
-<!-- L'en-tête -->
-<header>
-   <div id="centercolumn">
-      <p class="TitreC">FabLab On l'Fait</p>
-      <p class="SousTitreC">Apprendre, Partager, s'Amuser!</p>
-   </div><!-- #centercolumn -->
-</header>
