2346565a95dd5ff306d63c2121b1ad408f7ac1ac
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 06:15:11 +0200

    routes all page

--- www/state/routes.php
+++ www/state/routes.php
@@ -9,7 +9,55 @@ $PFM['routes'] = [
     'layout' => 'onlfait/default'
   ],
 
-  // error 404
+  // association routes
+  'association' => [
+    'title'  => 'Association',
+    'page'   => 'association/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  // contact routes
+  'contact' => [
+    'title'  => 'Contact',
+    'page'   => 'contact/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  // events routes
+  'events' => [
+    'title'  => 'Événements / Ateliers',
+    'page'   => 'events/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  // members routes
+  'members' => [
+    'title'  => 'Espace membres',
+    'page'   => 'members/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  'members/projects' => [
+    'title'  => 'Projets membres',
+    'page'   => 'members/projects',
+    'layout' => 'onlfait/default'
+  ],
+
+  // portfolio routes
+  'portfolio' => [
+    'title'  => 'Portfolio',
+    'page'   => 'portfolio/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  // workshop routes
+  'workshop' => [
+    'title'  => 'Espace de travail',
+    'page'   => 'workshop/index',
+    'layout' => 'onlfait/default'
+  ],
+
+  // errors routes
   '404' => [
     'title'  => 'Erreur 404',
     'page'   => 'errors/404',
