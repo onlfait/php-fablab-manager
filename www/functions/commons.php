ebc73fd3173ff86033901cc478ae7830a6d7eb86
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-18 05:49:46 +0200

    add commons functions to single file

---
+++ www/functions/commons.php
@@ -0,0 +1,41 @@
+<?php
+// require a file from the root path
+function pfm_require_file($file) {
+  global $PFM;
+  require PFM_ROOT_PATH . $file . '.php';
+}
+
+// require a page from pages directory
+function pfm_require_page($file) {
+  pfm_require_file('pages/' . $file);
+}
+
+// require a layout from layouts directory
+function pfm_require_layout($file) {
+  pfm_require_file('layouts/' . $file);
+}
+
+// Dispatch requested route
+function pfm_dispatch_route($request) {
+  global $PFM;
+
+  // Default route
+  $uri = '';
+
+  // Get the requested route
+  if (!empty($request['uri'])) {
+    $uri = trim($request['uri']);
+  }
+
+  // If the route does not exists
+  if (!isset($PFM['routes'][$uri])) {
+    $uri = '404';
+  }
+
+  // Update state
+  $PFM['route'] = $PFM['routes'][$uri];
+  $PFM['route']['uri'] = $uri;
+
+  // Include the page layout
+  pfm_require_layout($PFM['route']['layout']);
+}
