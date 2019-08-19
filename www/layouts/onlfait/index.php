5ff96d22c5d4be179cf6a5e556ec5f6fa9b5fe74
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-08-19 09:20:04 +0200

    clean layout base

--- www/layouts/onlfait/index.php
+++ www/layouts/onlfait/index.php
@@ -7,17 +7,11 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
     <title>Onl'Fait - <?php echo $PFM['route']['title'] ?></title>
-    <link rel="stylesheet" href="<?php echo pfm_url('layouts/onlfait/default.css') ?>">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo pfm_url('layouts/onlfait/styles/index.css') ?>">
   </head>
   <body>
-    <header>
-      <h1 class="title">FabLab On l'Fait</h1>
-      <p class="subtitle">Apprendre, Partager, s'Amuser!</p>
-    </header>
-    <nav>
-      <?php pfm_print_menu($PFM['menus']['main']) ?>
-    </nav>
-    <?php pfm_require_page($PFM['route']['page']) ?>
-    <script src="<?php echo pfm_url('layouts/onlfait/default.js') ?>"></script>
+    <!-- contents here ... -->
+    <script src="<?php echo pfm_url('layouts/onlfait/scripts/index.js') ?>"></script>
   </body>
 </html>
--- www/state/init.php
+++ www/state/init.php
@@ -2,7 +2,7 @@
 // Create the PFM state object (this is the only global)
 $PFM = [
   'route'  => null,
-  'layout' => 'onlfait/default',
+  'layout' => 'onlfait/index',
   'host'   => 'localhost',
   'https'  => false
 ];
