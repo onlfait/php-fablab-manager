078a9f16f6ae9a0b1c0fadadc5600c5f544c989a
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-09-06 07:16:28 +0200

    call session_start() once in index.php

--- www/index.php
+++ www/index.php
@@ -8,5 +8,8 @@ require_once PFM_ROOT_PATH . 'state/init.php';
 // Require commons functions
 require_once PFM_ROOT_PATH . 'functions/commons.php';
 
+// Start a session
+session_start();
+
 // Dispatch main request
 pfm_dispatch_route($_GET);
