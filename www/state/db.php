914094c223d3e6a9bb8328ab51fefc4cc4181786
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-09-06 18:10:12 +0200

    add db state

---
+++ www/state/db.php
@@ -0,0 +1,14 @@
+<?php
+// Database
+$PFM['db'] = [
+
+  // connection
+  'host' => 'localhost',
+  'user' => 'root',
+  'pass' => '1234',
+  'name' => 'php-fablab-manager_',
+
+  // connection link
+  'link' => null
+
+];
--- www/state/init.php
+++ www/state/init.php
@@ -10,3 +10,4 @@ $PFM = [
 // Require all state files
 require_once PFM_ROOT_PATH . 'state/routes.php';
 require_once PFM_ROOT_PATH . 'state/menus.php';
+require_once PFM_ROOT_PATH . 'state/db.php';
