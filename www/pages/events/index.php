52d46d55995c4765b17d0c629e43d122fc71be3c
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-09-07 17:16:28 +0200

    CSS picto

--- www/pages/events/index.php
+++ www/pages/events/index.php
@@ -13,20 +13,38 @@
 
   <h1 class="title">Evénements</h1>
 
+  <div class="grid">
+
   <?php while ($event = pfm_fetch_event($pfm_events)) { ?>
 
-    <h2 class="title"><?php echo $event->Titre ?> (<?php echo $event->id ?>)</h2>
-    <div><img class="responsive" src="Upload/EventImage/<?php echo $event->id?>.jpg" /></div>
-    <div>Lieu: <?php echo $event->Lieu ?></div>
-    <div>Début: <?php echo $event->HeureDebut ?></div>
-    <div>Fin: <?php echo $event->HeureFin ?></div>
+    <div class="cell cell-12 cell-desktop-6 pad">
 
-    <div><?php echo $event->Description ?></div>
-    <div><img src="Image/Picto/Atelier_<?php echo $event->Ateliers ?>.png" height="100"></div>
-    <div><img src="FonctionsPHP/ImageGD.php?text1=<?php echo $event->Age ?>ans&text2=Age min" alt="Age" height="100"></div>
-    <div><img src="FonctionsPHP/ImageGD.php?text1=<?php echo $event->PrixMembre ?>CHF&text2=Membre" alt="PrixMembre" height="100"></div>
-    <div><img src="FonctionsPHP/ImageGD.php?text1=<?php echo $event->PrixNonMembre ?>CHF&text2=Non-Membre" alt="PrixNonMembre" height="100"></div>
+      <h2 class="title"><?php echo $event->Titre ?> (<?php echo $event->id ?>)</h2>
+      <img class="responsive" src="Upload/EventImage/<?php echo $event->id?>.jpg" />
+
+      <div class="cell h-spacer height-x5"></div>
+
+      <div class="grid">
+        <div class="cell cell-12 cell-desktop-6">
+          <div><?php echo $event->Description ?></div>
+          <div>Lieu: <?php echo $event->Lieu ?></div>
+          <div>Début: <?php echo $event->HeureDebut ?></div>
+          <div>Fin: <?php echo $event->HeureFin ?></div>
+        </div>
+        <div class="cell cell-12 cell-desktop-6">
+          <div>
+            <?php pfm_print_picto_img('Atelier_' . $event->Ateliers) ?>
+            <?php pfm_print_picto('Age min.', $event->Age . 'ans', true) ?>
+            <?php pfm_print_picto('Age min.', $event->PrixMembre . ' CHF', true) ?>
+            <?php pfm_print_picto('Age min.', $event->PrixNonMembre . ' CHF', true) ?>
+          </div>
+        </div>
+      </div>
+
+    </div>
 
   <?php } // while => $event ?>
+
+  </div>
 
 </section>
