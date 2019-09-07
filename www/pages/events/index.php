7c5ce0c1a003dad2d33b7f3603952dc47a6f37f3
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-09-07 17:36:13 +0200

    print sujets and outils picto

--- www/pages/events/index.php
+++ www/pages/events/index.php
@@ -22,23 +22,37 @@
       <h2 class="title"><?php echo $event->Titre ?> (<?php echo $event->id ?>)</h2>
       <img class="responsive" src="Upload/EventImage/<?php echo $event->id?>.jpg" />
 
-      <div class="cell h-spacer height-x5"></div>
+      <div class="cell h-spacer height-x2"></div>
 
       <div class="grid">
-        <div class="cell cell-12 cell-desktop-6">
+
+        <div class="cell cell-12">
           <div><?php echo $event->Description ?></div>
           <div>Lieu: <?php echo $event->Lieu ?></div>
           <div>Début: <?php echo $event->HeureDebut ?></div>
           <div>Fin: <?php echo $event->HeureFin ?></div>
         </div>
+
+        <div class="cell h-spacer height-x2"></div>
+
         <div class="cell cell-12 cell-desktop-6">
-          <div>
-            <?php pfm_print_picto_img('Atelier_' . $event->Ateliers) ?>
-            <?php pfm_print_picto('Age min.', $event->Age . 'ans', true) ?>
-            <?php pfm_print_picto('Age min.', $event->PrixMembre . ' CHF', true) ?>
-            <?php pfm_print_picto('Age min.', $event->PrixNonMembre . ' CHF', true) ?>
-          </div>
+          <?php pfm_print_picto_img('Atelier_' . $event->Ateliers) ?>
+          <?php pfm_print_picto('Age min.', $event->Age . 'ans', true) ?>
+          <?php pfm_print_picto('Membre', $event->PrixMembre . ' CHF', true) ?>
+          <?php pfm_print_picto('Non-Membre', $event->PrixNonMembre . ' CHF', true) ?>
         </div>
+
+        <div class="cell cell-12 cell-desktop-6">
+        <?php
+          foreach ($event->Sujets as $row) {
+            pfm_print_picto_img('Sujet_' . $row->Sujets);
+          }
+          foreach ($event->Outils as $row) {
+            pfm_print_picto_img('Outil_' . $row->Outils);
+          }
+        ?>
+        </div>
+
       </div>
 
     </div>
