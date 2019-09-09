73d4b3936cbc1ce990088a5413d3128d1a362da0
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-09-09 09:22:50 +0200

    DRY abstract data from presentation

--- www/pfm/pages/workshop/index.php
+++ www/pfm/pages/workshop/index.php
@@ -1,93 +1,103 @@
+<?php
+// TODO puts this in the database
+function pfm_get_machines() {
+  return [
+    [
+      'name'        => 'Découpeuse Laser CO2',
+      'picture'     => pfm_public_url('images/machines/laser_cutter.jpg'),
+      'description' => 'Elle permet de découper/graver le bois, le plexiglass, le cuire.',
+      'details'     => [
+        'power'     => '90W',
+        'workspace' => '1000 x 600 mm',
+        'model'     => 'SignsTech / 1060SP',
+        'hardware'  => 'SmoothieBoard',
+        'software'  => 'LightBurn, Fabrica'
+      ]
+    ],
+
+    [
+      'name'        => 'Imprimante 3D',
+      'picture'     => pfm_public_url('images/machines/3d_printer.jpg'),
+      'description' => 'Deux imprimantes 3D sont à disposition.',
+      'details'     => [
+        'workspace' => '180 x 170 x 170 mm',
+        'model'     => 'RepRap / iTopie',
+        'hardware'  => 'SmoothieBoard',
+        'software'  => 'Cura, Pronterface'
+      ]
+    ],
+
+    [
+      'name'        => 'Fraiseuse numérique (CNC)',
+      'picture'     => pfm_public_url('images/machines/cnc.jpg'),
+      'description' => 'Elle permet d\'usiner du bois.',
+      'details'     => [
+        'workspace' => '800 x 700 x 150 mm',
+        'spindle'   => 'KRESS 1500',
+        'model'     => 'OpenBuilds OX',
+        'hardware'  => 'SmoothieBoard',
+        'software'  => 'CamBam, MeshCAM, Pronterface'
+      ]
+    ],
+
+    [
+      'name'        => 'Découpeuse Vinyle',
+      'picture'     => pfm_public_url('images/machines/vinyl_cutter.jpg'),
+      'description' => 'Pour la création de stickers, t-shirts, sacs, etc...',
+      'details'     => [
+        'workspace' => 'A3',
+        'model'     => 'Silhouette CAMEO 3',
+        'software'  => 'Silhouette Studio®'
+      ]
+    ],
+
+    [
+      'name'        => 'Thermoformeuse',
+      'picture'     => pfm_public_url('images/machines/vacuum_former.jpg'),
+      'description' => 'Pour la création de vos moules, coques, etc...',
+      'details'     => [
+        'workspace' => '220 x 180 mm',
+        'model'     => 'DIY'
+      ]
+    ]
+  ];
+}
+
+// TODO implements internatonalization
+function pfm_get_machines_label($label) {
+  static $labels = [
+    'power'     => 'Puissance',
+    'workspace' => 'Espace de travail',
+    'spindle'   => 'Broche',
+    'model'     => 'Modèle',
+    'hardware'  => 'Electronique',
+    'software'  => 'Logiciels'
+  ];
+  return isset($labels[$label]) ? $labels[$label] : $label;
+}
+?>
+
 <section>
-
   <h1 class="title">Espace de Travail</h1>
-  <h2 class="subtitle">Présentation des machines</h2>
-
+  <h2 class="subtitle">Les machines</h2>
   <div class="grid v-pad-x2">
-
+  <?php foreach (pfm_get_machines() as $machine): ?>
     <div class="cell cell-12 cell-tablet-4 h-pad-x2">
-      <img class="responsive"src="Image/Machine/DecoupeuseLaser.jpg">
+      <img class="responsive" src="<?php echo $machine['picture'] ?>" alt="<?php echo $machine['name'] ?>">
+      <div class="cell h-spacer height-x2"></div>
     </div>
-    <div class="cell cell-12 cell-tablet-8 pad">
-      <h2 class="title">Découpeuse Laser CO2</h2>
+    <div class="cell cell-12 cell-tablet-8 h-pad-x2">
+      <div class="text-bold"><?php echo $machine['name'] ?></div>
+      <?php echo $machine['description'] ?>
+      <div class="h-spacer height-x2"></div>
       <div class="grid">
-        <div class="cell cell-12">1 Découpeuse Laser est disponible. Elle permet de découper/graver le bois, le plexiglass, le cuire.</div>
-        <div class="h-spacer height-x2"></div>
-        <div class="cell cell-6 strong">Puissance</div><div class="cell cell-6">90W</div>
-        <div class="cell cell-6 strong">Espace de travail</div><div class="cell cell-6">1000 x 600 mm</div>
-        <div class="cell cell-6 strong">Marque / Modèle</div><div class="cell cell-6">SignsTech / 1060SP</div>
-        <div class="cell cell-6 strong">Electronique</div><div class="cell cell-6">SmoothieBoard (Open Hardware)</div>
-        <div class="cell cell-6 strong">Logiciel</div><div class="cell cell-6">LightBurn</div>
+      <?php foreach ($machine['details'] as $label => $value): ?>
+        <div class="cell cell-12 text-bold"><?php echo pfm_get_machines_label($label) ?></div>
+        <div class="cell cell-12"><?php echo $value ?></div>
+      <?php endforeach; ?>
       </div>
     </div>
-
     <div class="cell h-spacer height-x5"></div>
-
-    <div class="cell cell-12 cell-tablet-4 h-pad-x2">
-      <img class="responsive"src="Image/Machine/Imprimante3D.jpg">
-    </div>
-    <div class="cell cell-12 cell-tablet-8 pad">
-      <h2 class="title">Imprimantes 3D</h2>
-      <div class="grid">
-        <div class="cell cell-12">2 imprimantes 3D est à disposition. Elles possèdent un plateau chauffant et une buse de 1.75mm</div>
-        <div class="h-spacer height-x2"></div>
-        <div class="cell cell-6 strong">Espace de travail</div><div class="cell cell-6">220 x 220 x 260 mm</div>
-        <div class="cell cell-6 strong">Marque / Modèle</div><div class="cell cell-6">RepRap / iTopie (Open Hardware)</div>
-        <div class="cell cell-6 strong">Electronique</div><div class="cell cell-6">SmoothieBoard (Open Hardware)</div>
-        <div class="cell cell-6 strong">Logiciel</div><div class="cell cell-6">Slic3r, Pronterface, Fabrica, Cura</div>
-        <div class="cell cell-6 strong">Mode d'emploi</div><div class="cell cell-6">Tutorial</div>
-      </div>
-    </div>
-
-    <div class="cell h-spacer height-x5"></div>
-
-    <div class="cell cell-12 cell-tablet-4 h-pad-x2">
-      <img class="responsive"src="Image/Machine/CNC.jpg">
-    </div>
-    <div class="cell cell-12 cell-tablet-8 pad">
-      <h2 class="title">CNC (Fraiseuse numérique)</h2>
-      <div class="grid">
-        <div class="cell cell-12">1 Fraiseuse CNC est à disposition. Elle permet d'usiner du bois. </div>
-        <div class="h-spacer height-x2"></div>
-        <div class="cell cell-6 strong">Broche</div><div class="cell cell-6">KRESS 1500 </div>
-        <div class="cell cell-6 strong">Espace de travail</div><div class="cell cell-6">800 x 700 x 150 mm  </div>
-        <div class="cell cell-6 strong">Marque / Modèle</div><div class="cell cell-6">OpenBuilds / OX (Open Hardware)  </div>
-        <div class="cell cell-6 strong">Electronique</div><div class="cell cell-6">SmoothieBoard (Open Hardware)  </div>
-        <div class="cell cell-6 strong">Logiciel</div><div class="cell cell-6">LaserWeb, Fabrica (Open Source) – CamBam </div>
-      </div>
-    </div>
-
-    <div class="cell h-spacer height-x5"></div>
-
-    <div class="cell cell-12 cell-tablet-4">
-      <img class="responsive"src="Image/Machine/Decoupeuse.jpg">
-    </div>
-    <div class="cell cell-12 cell-tablet-8 pad">
-      <h2 class="title">Découpeuse Vinyle</h2>
-      <div class="grid">
-        <div class="cell cell-12">1 Découpeuse Vinyle est à disposition. </div>
-        <div class="h-spacer height-x2"></div>
-        <div class="cell cell-6 strong">Espace de travail</div><div class="cell cell-6">A3 </div>
-        <div class="cell cell-6 strong">Marque / Modèle</div><div class="cell cell-6">Silhouette CAMEO 3 </div>
-        <div class="cell cell-6 strong">Logiciel</div><div class="cell cell-6">Silhouette Studio® </div>
-      </div>
-    </div>
-
-    <div class="cell h-spacer height-x5"></div>
-
-    <div class="cell cell-12 cell-tablet-4">
-      Pas d'image pour le moment...
-    </div>
-    <div class="cell cell-12 cell-tablet-8 pad">
-      <h2 class="title">Thermoformeuse</h2>
-      <div class="grid">
-        <div class="cell cell-12">1 Thermoformeuse est à disposition. </div>
-        <div class="h-spacer height-x2"></div>
-        <div class="cell cell-6 strong">Espace de travail</div><div class="cell cell-6">220 x 180 mm  </div>
-        <div class="cell cell-6 strong">Marque / Modèle</div><div class="cell cell-6">Fait maison  </div>
-      </div>
-    </div>
-
+  <?php endforeach; ?>
   </div>
-
 </section>
